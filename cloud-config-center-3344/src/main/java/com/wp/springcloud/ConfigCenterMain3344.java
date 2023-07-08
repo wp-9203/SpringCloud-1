85bf62b4fe415e92d99500046a8f8cf6490b08c4
wp-9203 <wangpenga6@163.com>
2023-07-08 23:17:05 +0800

    服务配置 config 修改

--- cloud-config-center-3344/src/main/java/com/wp/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/wp/springcloud/ConfigCenterMain3344.java
@@ -3,6 +3,7 @@ package com.wp.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
